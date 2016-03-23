cf7084142194472b90612d4bc7ace9080398b0d2
KuraKuraDave <kurakuradave@gmail.com>
2016-03-23 16:47:22 +0800

    tweaked extraction to only grab latest news titles

--- KompasMinutes.sh
+++ KompasMinutes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget -O front.html www.kompas.com
-sed -i 's/var more.*$//g' front.html
-grep -Poi 'read.*\>\K.*(?=<\/a)' front.html > text.txt
+grep -Poi 'latest__title.*?\K.*?(?=latest__group)' front.html > s1.txt
+grep -Poi 'kompas.com\\/read.*\">\K.*?(?=<)' s1.txt > text.txt
+sed -i 's/\\"//g' text.txt 
 sed -i 's/$/./g' text.txt
