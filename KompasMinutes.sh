616fa0ce83778ec98d644c37d2224251bb2e3d10
David Effendi <kurakuradave@gmail.com>
2017-04-15 09:33:16 +0800

    Adjusted extraction pattern to match Kompas' new format Apr 2017

--- KompasMinutes.sh
+++ KompasMinutes.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 wget -O front.html www.kompas.com
-grep -Poi 'latest__title.*?\K.*?(?=latest__group)' front.html > s1.txt
-grep -Poi 'kompas.com\\/read.*\">\K.*?(?=<)' s1.txt > text.txt
-sed -i 's/\\"//g' text.txt 
-sed -i 's/$/./g' text.txt
+#grep -Poi 'latest__title.*?\K.*?(?=latest__group)' front.html > s1.txt
+#grep -Poi 'kompas.com\\/read.*\">\K.*?(?=<)' s1.txt > text.txt
+#sed -i 's/\\"//g' text.txt 
+#sed -i 's/$/./g' text.txt
+
+#Adjusted to suit Kompas' new format (15 Apr 2017)
+grep -Poi "h1.*big__title.*\>\K.*(?=\<)" front.html > text.txt
+grep -Poi "kompas\.com\/read.*?>\K.*(?=\<\/a)" front.html >> text.txt
+sed -i '/^<img.*$/d' text.txt
+
 #espeak -f text.txt -s 250 -v id
