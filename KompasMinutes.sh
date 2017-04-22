311ad3086cb6c7b615ecabcdbe6f2d99d78ef45a
David Effendi <kurakuradave@gmail.com>
2017-04-22 10:52:16 +0800

    Added crawling of newsjs.com's contents
    Added run.sh

--- KompasMinutes.sh
+++ KompasMinutes.sh
@@ -7,8 +7,13 @@ wget -O front.html www.kompas.com
 
 #Adjusted to suit Kompas' new format (15 Apr 2017)
 grep -Poi "h1.*big__title.*\>\K.*(?=\<)" front.html > text.txt
-grep -Poi "kompas\.com\/read.*?>\K.*(?=\<\/a)" front.html >> text.txt
+grep -Poi "kompas\.com\/read.*?>\K.*(?=\<\/a)" front.html > text.txt
 sed -i '/^<img.*$/d' text.txt
+
+#Adding newsjs.com content
+wget www.newsjs.com/id/ -O front_newsjs.html
+grep -Poi "(\<a href.*newsjs.com\/id\/.*bookmark\"\>\K.*(?=\<\/a\>)|\<p\>\K.*(?=\<\/p\>))" front_newsjs.html >> text.txt
+
 sed -i 's/$/<Break time="1000ms"\/>/g' text.txt #comment out this line if you dont want ssml break tag
 
 #espeak -m -f text.txt -s 250 -v id
---
+++ run.sh
@@ -0,0 +1,5 @@
+!#/bin/bash
+while true; do
+    sh ./KompasMinutes.sh
+    sleep 300
+done
