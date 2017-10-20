247ed39765b563a792e76f5e067f3f97694814ed
David Effendi <kurakuradave@gmail.com>
2017-10-20 17:27:18 +0800

    Fixed article title extraction to suit kompas.com's new structure

--- KompasMinutes.sh
+++ KompasMinutes.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-wget -O front.html www.kompas.com
+wget -O front.html http://nasional.kompas.com/
 #grep -Poi 'latest__title.*?\K.*?(?=latest__group)' front.html > s1.txt
 #grep -Poi 'kompas.com\\/read.*\">\K.*?(?=<)' s1.txt > text.txt
 #sed -i 's/\\"//g' text.txt 
 #sed -i 's/$/./g' text.txt
+
+grep -Poi "_parent.>\K.*(?=</a)" front.html > text.txt
 
 #Adjusted to suit Kompas' new format (15 Apr 2017)
 grep -Poi "h1.*big__title.*\>\K.*(?=\<)" front.html > text.txt
@@ -16,4 +18,4 @@ grep -Poi "(\<a href.*newsjs.com\/id\/.*bookmark\"\>\K.*(?=\<\/a\>)|\<p\>\K.*(?=
 
 sed -i 's/$/<Break time="1000ms"\/>/g' text.txt #comment out this line if you dont want ssml break tag
 
-#espeak -m -f text.txt -s 250 -v id
+espeak -m -f text.txt -s 350 -v id
